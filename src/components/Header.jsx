84311f08085e75fa74d1d11aa71cda52245d0237
phatommobb <nickoluoch1130@gmail.com>
2025-05-26 13:44:00 +0300

    adding assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header-extensions">
+        <div className="header-logo">Extensions</div>
+      </div>
+      <div className="header-theme-icon"></div>
+    </div>
+  );
 }
 
 export default Header;
