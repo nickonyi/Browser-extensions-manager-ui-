48fa803907bd8ac2f4c50ab6c6c20dd15149be7e
phatommobb <nickoluoch1130@gmail.com>
2025-05-26 14:14:22 +0300

    Revert "adding assets"
    This reverts commit e8f8c304c0285528dec9b049f7665747aad1578f.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg';
-import themeIcon from '../assets/images/icon-moon.svg';
 
 function Header() {
   return (
     <div className="header">
       <div className="header-extensions">
-        <div className="header-logo">
-          <img src={logo} alt="logo" />
-        </div>
+        <div className="header-logo">Extensions</div>
       </div>
-      <div className="header-theme-icon">
-        <img src={themeIcon} alt="theme icon" />
-      </div>
+      <div className="header-theme-icon"></div>
     </div>
   );
 }
