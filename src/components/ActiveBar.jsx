66f00722cc36106f4ca5a6e3ec46b60e29ec384a
phatommobb <nickoluoch1130@gmail.com>
2025-05-26 14:34:13 +0300

    styling the theme icon

--- src/components/ActiveBar.jsx
+++ src/components/ActiveBar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 function ActiveBar() {
-  return <div>ActiveBar</div>;
+  return (
+    <div className="active-bar-container">
+      <div className="active-bar-text">
+        <p className="active-bar-text-title">Extensions List</p>
+      </div>
+      <div className="active-bar-buttons">
+        <button className="active-bar-button active">All</button>
+        <button className="active-bar-button">Active</button>
+        <button className="active-bar-button">Inactive</button>
+      </div>
+    </div>
+  );
 }
 
 export default ActiveBar;
